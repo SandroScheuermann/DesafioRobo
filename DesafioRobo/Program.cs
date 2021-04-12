0d1a0d6ca607a6f0c451b9b06d25f6005a352660
SandroScheuermann <sandroryze@gmail.com>
2021-04-11 23:10:41 -0300

    VFinal

--- DesafioRobo/Program.cs
+++ DesafioRobo/Program.cs
@@ -10,10 +10,29 @@ namespace DesafioRobo
     {
         static void Main(string[] args)
         {
+            string posInicial = "";
+            string movimento = "";
 
+            Console.WriteLine("Digite a dimensão(X,Y) do grid :");
+            string grid = Console.ReadLine();
 
+            while (true)
+            {
 
+                Console.WriteLine("\nDigite a posição inicial do robô :");
 
+                posInicial = Console.ReadLine();
+
+                Robo robo = new Robo(grid, posInicial);
+
+                Console.WriteLine("\nDigite o movimento do robô :");
+
+                movimento = Console.ReadLine();
+
+                robo.Mover(movimento);
+
+                robo.MostrarResultado();
+            }
         }
     }
 }
---
+++ DesafioRobo/Robo.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioRobo
+{
+    class Robo
+    {
+
+        string grid = "", faceAtual = "", posInicial = "";
+        int posX, posY;
+        string[] gridDados;
+        string[] posInicialDados;
+
+        public Robo(string grid, string posInicial)
+        {
+            this.posInicial = posInicial;
+            this.grid = grid;
+
+        }
+
+        public void Mover(string movimento)
+        {
+
+            gridDados = grid.Split(' ');
+            posInicialDados = posInicial.Split(' ');
+            this.posX = Int16.Parse(posInicialDados[0]);
+            this.posY = Int16.Parse(posInicialDados[1]);
+            this.faceAtual = posInicialDados[2];
+
+
+            for (int i = 0; i < movimento.Length; i++)
+            {
+                if (movimento[i].Equals('e') || movimento[i].Equals('E'))
+                {
+
+                    if (this.faceAtual.Equals("n", StringComparison.OrdinalIgnoreCase))
+                    {
+                        this.faceAtual = "O";
+
+                    }
+                    else if (this.faceAtual.Equals("o", StringComparison.OrdinalIgnoreCase))
+                    {
+                        this.faceAtual = "S";
+
+                    }
+                    else if (this.faceAtual.Equals("s", StringComparison.OrdinalIgnoreCase))
+                    {
+                        this.faceAtual = "L";
+
+                    }
+                    else if (this.faceAtual.Equals("l", StringComparison.OrdinalIgnoreCase))
+                    {
+                        this.faceAtual = "N";
+
+                    }
+                }
+
+                else if (movimento[i].Equals('d') || movimento[i].Equals('D'))
+                {
+
+                    if (this.faceAtual.Equals("n", StringComparison.OrdinalIgnoreCase)) 
+                    {
+                        this.faceAtual = "L";
+
+                    }
+                    else if (this.faceAtual.Equals("o", StringComparison.OrdinalIgnoreCase))
+                    {
+                        this.faceAtual = "N";
+
+                    }
+                    else if (this.faceAtual.Equals("s", StringComparison.OrdinalIgnoreCase))
+                    {
+                        this.faceAtual = "O";
+
+                    }
+                    else if (this.faceAtual.Equals("l", StringComparison.OrdinalIgnoreCase))
+                    {
+                        this.faceAtual = "S";
+
+                    }
+                }
+
+                else if (movimento[i].Equals('m') || movimento[i].Equals('M')) 
+                {
+
+                    if (this.faceAtual.Equals("n", StringComparison.OrdinalIgnoreCase))
+                    {
+                        this.posY++;
+
+                    }
+                    if (this.faceAtual.Equals("s", StringComparison.OrdinalIgnoreCase))
+                    {
+                        this.posY--;
+
+                    }
+                    if (this.faceAtual.Equals("l", StringComparison.OrdinalIgnoreCase))
+                    {
+                        this.posX++;
+
+                    }
+                    if (this.faceAtual.Equals("o", StringComparison.OrdinalIgnoreCase))
+                    {
+                        this.posX--;
+
+                    }
+                }
+            }
+        }
+
+        public void MostrarResultado()
+        {
+            Console.Write("\n" +this.posX.ToString() + " " + this.posY.ToString() + " " + this.faceAtual + "\n");
+
+        }
+
+    }
+
+
+
+
+
+}
